6bb91b7adb38e07e51bb46b85052c3a91f61bf0b
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-02 15:40:40 -0700

    setting up testing enviroment for mock API

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,21 +3,31 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
+require 'webmock/minitest'
+require 'vcr'
 
-#  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
   ENV,
   Minitest.backtrace_filter
 )
 
-
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
-
-# Uncomment for awesome colorful output
-# require "minitest/pride"
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  # filter_sensitive_data is a method that comes from the VCR gem
+  config.filter_sensitive_data("<KEY>") do
+    ENV['EDAMAM_KEY']
+  end
+  config.filter_sensitive_data("<ID>") do
+    ENV['EDAMAM_ID']
+  end
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
