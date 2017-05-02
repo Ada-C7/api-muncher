80c8bd9fa33d9ae318635269e86be35e09e856eb
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-02 14:28:49 -0700

    added view files

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,18 @@
 class RecipesController < ApplicationController
 
+  # can be homepage
   def index
   end
 
-  def new
+  # can be search page
+  def search
   end
 
+  # will take data from search and call the lib API method
   def create
   end
 
+  # will show results from search
   def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
-  resources :recipes only: [:index, :new, :create, :show]
+  resources :recipes only: [:index, :create, :show]
+  get 'recipes/search', to: 'recipes#search', as: 'recipe_search'
 
 end
