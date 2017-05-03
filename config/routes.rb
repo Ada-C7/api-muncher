752b8dbc9ed615580009d5d1d942b5c82128b35a
Kayla <kubicke.kayla@gmail.com>
2017-05-03 11:39:13 -0700

    User can search for a term and land on an index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'recipes#root'
+
+  get 'recipes', to: 'recipes#index', as: 'recipes'
+  post 'recipes', to: 'recipes#search_recipes', as: 'search_recipes'
+
 end
