82170f699d2b7b5a48e006070d2b246c9fbfad4e
amb54 <ambarklund@hotmail.com>
2017-05-03 10:45:48 -0700

    Added three routes for the actions index, show and search.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get '/recipes', to: 'recipes#index', as: 'recipes'
+get '/recipes/:recipe/show', to: 'recipes#show', as: 'recipe'
+get '/recipes/:search_text', to: 'recipe#search', as: 'search'
+
 end
