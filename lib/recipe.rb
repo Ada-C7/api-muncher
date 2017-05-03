c59d5ea2dc94b3ffef4b8b32a2e37b6b4905a77f
Cara Comfort <cara.comfort10@gmail.com>
2017-05-03 11:34:58 -0700

    Implement initialize instance method and search class method for recipe class

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,40 @@
 require 'httparty'
 
 class Recipe
+  BASE_URL = "https://api.edamam.com/"
   ID = ENV["EDAMOM_ID"]
   KEY = ENV["EDAMOM_KEY"]
+
+  attr_reader :id, :name, :image, :source_url, :ingredients\
+
+  def initialize(data)
+    @id = data["uri"].match(/#recipe\_(.*)/)[1]
+    @name = data["label"]
+    @image = data["image"]
+    @source_url = data["url"]
+    @ingredients = data["ingredientLines"]
+  end
+
+  # returns a collection of recipe objects that match the search conditions
+  # if no range (to:, from:) is provided, defaults to first ten
+  def self.search(search_term)
+    query_params = {
+      "q" => search_term,
+      "app_id" => ID,
+      "app_key" => KEY,
+      "from" => 0,
+      "to" => 2
+    }
+
+    url = "#{BASE_URL}search"
+    response = HTTParty.get(url, query: query_params)
+    if response.code == 200
+      recipes = response["hits"].map do |recipe_data|
+        self.new(recipe_data["recipe"])
+      end
+      return recipes
+    else
+      return response.code
+    end
+  end
 end
