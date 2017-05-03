b90111fb90ae808c14986de2fdea1baff38eaa38
Cara Comfort <cara.comfort10@gmail.com>
2017-05-03 15:27:18 -0700

    Add functionality to return to the results page after viewing a single recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,10 +6,12 @@ class RecipesController < ApplicationController
 
   def index
     @recipes = Recipe.search("q" => params["q"])
+    @search_term = params["q"]
   end
 
   def show
     @recipe = Recipe.search("q" => params["id"]).first
+    @search_term = params["q"]
   end
 
   #add strong params
