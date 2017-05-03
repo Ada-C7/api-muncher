f10f24e15a27d6e64bdbd91e22385dd6ce14d0e1
Spatterjaaay <aja.pursova@gmail.com>
2017-05-03 15:06:44 -0700

    adjusted listing recipes method, wrote part of the individual recipe method

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -2,29 +2,32 @@ require 'httparty'
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search"
-  TOKEN = ENV["EDAMAM_TOKEN"]
+  APP_KEY = ENV["EDAMAM_TOKEN"]
   APP_ID = dc26f0f1
 
   def self.listRecipes(search_term)
     token ||= TOKEN
-    url = BASE_URL + "?g=#{search_term.gsub(" ", "%20")}&" + APP_ID + "&" + TOKEN
+    url = BASE_URL + "?g=#{search_term.gsub(" ", "%20")}&" + APP_ID + "&" + APP_KEY
 
     response = HTTParty.get(url)
 
     recipes = []
-    if response["recipes"]
-      response["reicpes"].each do |recipe|
-        label = recipe["label"]
-        image = recipe["image"]
-        uri = recipe["uri"]
-        url = recipe["url"]
+    if response["hits"]
+      response["hits"].each do |hit|
+        label = hit["recipe"]["label"]
+        image = hit["recipe"]["image"]
+        uri = hit["recipe"]["uri"]
+        url = hit["recipe"]["url"]
 
         recipes << Recipe.new(label, image, uri, url)
       end
     end
 
-    return channels
-    end
+    return recipes
   end
+
+def self.findRecipe(uri)
+  url = BASE_URL + "r=%23#{uri}&" + APP_ID + "&" + APP_KEY
+end
 
 end
