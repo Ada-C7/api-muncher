a256f44951be02ceb2a3764796fef5a8b0aa79ee
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-03 14:20:43 -0700

    changed search name, need to make controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   root 'search#index'
 
+  get ':recipe/index', to: 'homepages#index', as: 'index_recipe'
+
+  post ':recipe', to: 'homepages#create', as: 'create_message'
+
 end
