6d26dc746ebac55100901acef69dad67e020d9d6
amb54 <ambarklund@hotmail.com>
2017-05-03 10:56:10 -0700

    Setup for test of lib/edamam_recipe.

---
+++ test/lib/edamam_recipe_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+
+describe EdamamRecipe do
+
+
+  describe "initialize" do
+
+  end #END of describe "initialize"
+
+  describe "self.search" do
+
+  end # END of describe "self.search"
+
+  describe "self.all" do
+
+  end # END of describe "self.all"
+
+  describe "self.find" do
+
+  end # END of describe "self.find"
+
+  describe "show" do
+
+  end # END of describe "show"
+
+
+end # END of describe SlackChannel
