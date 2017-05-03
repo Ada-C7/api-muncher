46463914088d509ff6b1ab407513114c03086d5b
Regan Huff <regan.huff@gmail.com>
2017-05-03 12:04:14 -0700

    Attempt to use will_paginate gem to paginate

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,8 @@ class RecipesController < ApplicationController
   def index
 
     if params[:search_term]
-      @recipes =  EdamamWrapper.getRecipes(params[:search_term])
+      @recipe_results =  EdamamWrapper.getRecipes(params[:search_term])
+      @recipes = @recipe_results.paginate(:page => params[:page], :per_page => 10)
     end
   end
 
