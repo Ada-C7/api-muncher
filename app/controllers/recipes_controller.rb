c1dc6bf59ccf96edd72c86c9897c21f0499e15e5
Andrea Valliere <amvalliere@gmail.com>
2017-05-03 12:39:02 -0700

    Index method added to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,20 +3,7 @@ require '.env'
 
 class RecipesController < ApplicationController
   def index
-  end
-
-  def search(search_term)
-    query_params = {
-      "token" => TOKEN,
-      "channel" => @name,
-      "text" => message,
-      "username" => "Roberts-Robit",
-      "icon_emoji" => ":robot_face",
-      "as_user" => "false"
-    }
-    results = HTTParty.get("https://api.edamam.com/search?q=#{search_term}&app_id=${EDAMAM_ID}&app_key=${EDAMAM_TOKEN}&from=0&to=3")
-
-    return results
+    RecipesApiWrapper.search(query)
   end
 
   def show
