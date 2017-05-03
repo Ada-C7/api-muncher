3fa35eef09d12d122901a1cdf0ed6f0c0c206850
Bo Trethewey <botrethewey@gmail.com>
2017-05-03 10:06:52 -0700

    set up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'homepages#search'
+  get 'recipes', to: 'homepages#list', as: 'result_list'
+  get 'recipes/:recipe_id', to: "homepages#show", as: 'recipe_details'
+
 end
