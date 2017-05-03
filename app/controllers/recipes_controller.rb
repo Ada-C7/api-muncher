98926be60b10298595e7a56f7af79080928a1f2f
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-03 12:41:09 -0700

    refracted the the route from search to search results

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,16 +3,8 @@ class RecipesController < ApplicationController
 
   def index
     new_search = EdamamSearch.new(params[:search_text])
-    @results = new_search.search_results    
+    @results = new_search.search_results
     # raise
-  end
-
-  def search
-  end
-
-  # will take data from search and call the lib API method
-  def search_results
-    redirect_to recipes_path(params[:search_text])
   end
 
   # will show results from search
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
-  post 'results', to: 'recipes#search_results', as: 'get_results'
   get 'search', to: 'recipes#search', as: 'recipe_search'
-  get 'recipes/:search_text', to: 'recipes#index', as: 'recipes'
+  get 'recipes', to: 'recipes#index', as: 'recipes'
   resources :recipes, only: [:show]
-
 
 end
