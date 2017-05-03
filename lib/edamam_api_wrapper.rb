2e66f706bff4a1eac48601322cbebc81b87d4047
Spatterjaaay <aja.pursova@gmail.com>
2017-05-03 14:48:15 -0700

    added list recipe wrapper method, and adjusted recipe intialize method

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -3,8 +3,28 @@ require 'httparty'
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search"
   TOKEN = ENV["EDAMAM_TOKEN"]
+  APP_ID = dc26f0f1
 
   def self.listRecipes(search_term)
+    token ||= TOKEN
+    url = BASE_URL + "?g=#{search_term.gsub(" ", "%20")}&" + APP_ID + "&" + TOKEN
+
+    response = HTTParty.get(url)
+
+    recipes = []
+    if response["recipes"]
+      response["reicpes"].each do |recipe|
+        label = recipe["label"]
+        image = recipe["image"]
+        uri = recipe["uri"]
+        url = recipe["url"]
+
+        recipes << Recipe.new(label, image, uri, url)
+      end
+    end
+
+    return channels
+    end
   end
 
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,9 +1,11 @@
 class Recipe
   attr_reader :name, :id
 
-  def initiaize(name, id)
+  def initialize(label, image, uri, url)
     raise ArgumentError if name == nil || name == "" || id == nil || id == ""
-    @name = name
-    @id = id
+    @name = label
+    @image_url = image
+    @id = uri
+    @url = url
   end
 end
