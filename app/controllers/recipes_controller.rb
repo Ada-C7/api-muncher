044a04abdfd150268c6654e545578aad28cb3ac6
Anacelia Gillette <acgillette426@gmail.com>
2017-05-03 14:58:01 -0700

    Finished pagination logic besides edge cases yay

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,12 +5,12 @@ class RecipesController < ApplicationController
 
   def recipes
     @ingredient = params[:search]
-    if !(params[:page])
+    if (params[:page].to_i == 1)
       @recipes = EdamamApiWrapper.findRecipes(params[:search])[0]
       @page = 1
     else
-      @page = params[:page].to_i 
-      from = @page * 10
+      @page = params[:page].to_i
+      from = (@page * 10) - 10
       @recipes = EdamamApiWrapper.findRecipes(params[:search], from)[0]
     end
   end
