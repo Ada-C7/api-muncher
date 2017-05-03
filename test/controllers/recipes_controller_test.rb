3d3a7a6261703ac213dc2b43fbfefdaaf35bb936
Regan Huff <regan.huff@gmail.com>
2017-05-03 11:08:11 -0700

    Add tests for recipe index controller action

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  it "should get index with no search term" do
+    VCR.use_cassette("recipes") do
+      get root_path
+      must_respond_with :success
+    end
+  end
+
+  it "should also get index with the results of a search" do
+    VCR.use_cassette("recipes") do
+      search_data = {
+        search_term: "banana"
+      }
+      get root_path, params: search_data
+      must_respond_with :success
+    end
+  end
+
+  
 end
