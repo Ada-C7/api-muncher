9ef5ec47ec9e07c68445343beba6de95d25726c0
Kayla <kubicke.kayla@gmail.com>
2017-05-03 11:52:15 -0700

    A search term can be entered by user and the user will be directed to the index view.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,15 @@
+require'recipe' #FIXME: just changed
+
 class RecipesController < ApplicationController
 
-  def index
+  def root; end
+
+  def index; end
+
+  def search_recipes
     @recipes = Recipe.search(params[:search])
+
+    redirect_to recipes_path
   end
 
 end
