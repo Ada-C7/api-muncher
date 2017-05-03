87f8e7eee5372aeb7c4bb6f2d9798bb455122191
Brenna <bcmdarroch@gmail.com>
2017-05-03 13:58:56 -0700

    started recipe tests

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,5 +1,47 @@
 require 'test_helper'
 
 describe Recipe do
-  
+
+  before do
+    @recipe_hash = {
+      "id" => "recipe.com/recipe/ejfajfdn",
+      "label" => "most delicious recipe",
+      "image" => "recipe.com/recipe.jpg",
+      "url" => "recipe.com/recipe",
+      "ingredientLines" => ["yummyness", "chocolate bits", "savoury sweet flavor"],
+      "calories" => 1000000,
+      "fat" => 0.0001,
+      "sugar" => 0.0000001,
+      "protein" => 7000,
+      "sodium" => 50
+    }
+  end
+
+  it "requires a hash argument to initialize" do
+    proc{
+      Recipe.new
+    }.must_raise ArgumentError
+
+    proc{
+      Recipe.new "recipe 1"
+    }.must_raise ArgumentError
+  end
+
+  it "can create a Recipe instance" do
+    recipe = Recipe.new(@recipe_hash)
+
+    recipe.class.must_equal Recipe
+  end
+
+  it "has all accessor methods" do
+    # recipe = Recipe.new "recipe 1", 1, {   purpose: "to be number 1", is_archived: false, is_general: true, members: "Brenna, Kelly, Olivia" }
+    #
+    # recipe.name.must_equal "recipe 1"
+    # recipe.id.must_equal 1
+    # recipe.purpose.must_equal "to be number 1"
+    # recipe.is_archived.must_equal false
+    # recipe.is_general.must_equal true
+    # recipe.members.must_equal "Brenna, Kelly, Olivia"
+    # end
+  end
 end
