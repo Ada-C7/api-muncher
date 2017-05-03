8fa3aa1610d6725d1793f98fa6656cf49f3464c9
Cara Comfort <cara.comfort10@gmail.com>
2017-05-03 11:41:10 -0700

    Create recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'recipes#search'
 end
+
+
+# def root
+#   @albums = Work.best_albums
+#   @books = Work.best_books
+#   @movies = Work.best_movies
+#   @best_work = Work.order(vote_count: :desc).first
+# end
