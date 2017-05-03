93b60ef260abff57040523f9ee208620f092cfa4
add2point71dots <alcland@gmail.com>
2017-05-03 11:23:02 -0700

    added path names to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/recipes/:search', to: 'recipes#index'
+
+  get '/recipes/:search', to: 'recipes#index', as: 'recipes'
+  get '/recipes/:uri', to: 'recipes#show', as: 'recipe'
 end
