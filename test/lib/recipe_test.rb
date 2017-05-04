2b7d51ed86e897f3cd49343d676fcf1359b4d236
Janice Lichtman <jalichtman@gmail.com>
2017-05-04 10:01:03 -0700

    have test for search success case

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 describe Recipe do
 
+
+  let(:recipe_params) { {name: "food", image:"picture of food", id: 2345, original_url: "url"} }
+  let(:new_recipe) {Recipe.new(recipe_params)}
+
   describe "initialize" do
-
     it "Takes a hash, and assigns name, image, id, original_url" do
-
-      recipe_params = {name: "food", image:"picture of food", id: 2345, original_url: "url"}
-      recipe = Recipe.new(recipe_params)
-      recipe.name.must_equal recipe_params[:name]
+      new_recipe.name.must_equal recipe_params[:name]
     end
 
     it "Requires a hash" do
@@ -17,5 +17,28 @@ describe Recipe do
       }.must_raise ArgumentError
     end
   end
+
+  describe "search" do
+    it "Can search for recipes based on a search term, and returns and array of recipes" do
+      VCR.use_cassette("recipes") do
+        recipes = Recipe.search("blueberry")
+        recipes.class.must_equal Array
+
+        recipes.each do |recipe|
+          recipe.class.must_equal Recipe
+        end
+      end
+    end
+  end
+    #   it "Fails to send to a bogus channel" do
+    #     VCR.use_cassette("channels") do
+    #       channel = SlackChannel.new('this_channel_doesnt_exist')
+    #       proc {
+    #         channel.send("test message")
+    #       }.must_raise SlackChannel::SlackException
+    #     end
+    #   end
+    # end
+
 
 end
