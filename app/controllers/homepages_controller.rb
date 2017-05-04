0c6f86bd31bc78f2334c12ddc8e4263f74da51fc
Olivia <ssecretsharer@gmail.com>
2017-05-04 15:05:25 -0700

    installed foundation, put in preliminary styling. last push before new branch

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,11 +1,12 @@
-require 'will_paginate'
+require 'will_paginate/array'
 
 class HomepagesController < ApplicationController
 
   def index
     recipes = EdamamApiWrapper.search(params[:search_term])
-
-    @search_recipes = recipes.paginate(:page => params[:page], :per_page => 10)
+    if recipes.length > 0
+      @search_recipes = recipes.paginate(:page => params[:page], :per_page => 10)
+    end
   end
 
 end
--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -6,7 +6,6 @@ class EdamamApiWrapper
   APP_KEY = ENV["EDAMAM_KEY"]
 
   def self.search(search_term)
-    # raise
     url = BASE_URL + "search?" + "app_id=#{APP_ID}&" + "app_key=#{APP_KEY}&" + "q=#{search_term}&" + "to=100"
 
     response = HTTParty.get(url)
@@ -23,4 +22,21 @@ class EdamamApiWrapper
     end
     return recipes
   end
+
+  # def self.show_recipe(uri)
+  #   response = HTTParty.get(url)
+  #
+  #   recipes = []
+  #   if response["hits"]
+  #
+  #     response["hits"].each do |recipe|
+  #       label = recipe["recipe"]["label"]
+  #       uri = recipe["recipe"]["uri"]
+  #       image = recipe["recipe"]["image"]
+  #       recipes << Recipe.new(label, uri, image)
+  #     end
+  #   end
+  #   return recipes
+
+
 end
