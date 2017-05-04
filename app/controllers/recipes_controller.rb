f0fd53159582988cb7c91c81727e47177c13a9ce
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-05-04 09:09:06 -0700

    added dependency requirement

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,5 @@
+require_dependency '../../lib/recipe'
+
 class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all_by_search_term(params[:search_term], params[:from], params[:to])
