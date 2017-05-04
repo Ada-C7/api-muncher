c7356873b6879628bd3c59169db66d8048899130
Cara Comfort <cara.comfort10@gmail.com>
2017-05-04 09:10:55 -0700

    Add links on index to display the next ten search results

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,13 +5,16 @@ class RecipesController < ApplicationController
   end
 
   def index
-    @recipes = Recipe.search("q" => params["q"])
     @search_term = params["q"]
+    @recipes = Recipe.search("q" => params["q"], "to" => params["to"], "from" => params["from"])
+    count = Recipe.count_results(params["q"])
+    @to = (params["to"] ||= 10).to_i
+    @to += 10 if count > @to
   end
 
   def show
-    @recipe = Recipe.search("q" => params["id"]).first
     @search_term = params["q"]
+    @recipe = Recipe.search("q" => params["id"]).first
   end
 
   #add strong params
