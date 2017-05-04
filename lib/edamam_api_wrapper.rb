3cd241e2f6cd5b7bb9385f6a7c9add2ba9582361
Bo Trethewey <botrethewey@gmail.com>
2017-05-04 00:10:16 -0700

    added search feature: still working on logic for the next 10 items

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -5,22 +5,29 @@ class EdamamApiWrapper
   EDAMAM_ID = ENV["EDAMAM_ID"]
   EDAMAM_KEY = ENV["EDAMAM_KEY"]
 
-# method that takes search term, desired beginning and ending index
-# and returns an array of recipe objects
-# returns an empty array if search finds no match
-  def self.searchRecipes(q, from, to)
-    edamam_id ||= EDAMAM_ID
-    edamam_key ||= EDAMAM_KEY
-
-    url = BASE_URL + "?q=#{q.gsub(" ", "%20")}" + "&app_id=#{edamam_id}" + "&app_key=#{edamam_key}" + "&from=#{from}" + "&to=#{to}"
+  # method that takes search term, desired beginning and ending index
+  # and returns an array of recipe objects
+  # returns an empty array if search finds no match
+  def self.searchRecipes(q, from)
+    to = from + 10
+    url = BASE_URL + "?q=#{q.gsub(" ", "%20")}" + "&app_id=#{EDAMAM_ID}" + "&app_key=#{EDAMAM_KEY}" + "&from=#{from}" + "&to=#{to}"
     recipes = []
     response = HTTParty.get(url)
     if response["count"] > 0
       response["hits"].each do |recipe_hash|
-        recipes << Recipe.new(recipe_hash["recipe"]["label"],recipe_hash["recipe"]["uri"].gsub("#","%23"),recipe_hash["recipe"]["image"])
+        recipes << Recipe.new(recipe_hash["recipe"])
       end
     end
     return recipes
   end
 
+  # method that takes an uri of a specific recipe
+  # and returns a recipe object
+  def self.getRecipe(uri)
+    url = BASE_URL + "?r=#{uri.gsub("#","%23")}"+ "&app_id=#{EDAMAM_ID}" + "&app_key=#{EDAMAM_KEY}"
+    response = HTTParty.get(url)
+    recipe = Recipe.new(response[0])
+   raise
+    return recipe
+  end
 end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,9 +1,15 @@
 class Recipe
-  attr_reader :label, :uri, :img_url
-  def initialize(label, uri, img_url)
-    raise ArgumentError if label == nil || label == "" || uri == nil || uri == "" || img_url == nil || img_url == ""
-      @label = label
-      @uri = uri
-      @img_url = img_url
+  attr_reader :label, :uri, :image, :url, :ingredients, :totalNutrients, :totalDaily
+
+  def initialize(recipe_hash)
+    raise ArgumentError if recipe_hash["label"] == nil || recipe_hash["label"] == "" || recipe_hash["uri"] == nil || recipe_hash["uri"] == ""
+
+    @uri = recipe_hash["uri"]
+    @label = recipe_hash["label"]
+    @image = recipe_hash["image"]
+    @url = recipe_hash["url"]
+    @ingredients = recipe_hash["ingredients"]
+    @totalNutrients = recipe_hash["totalNutrients"]
+    @totalDaily = recipe_hash["totalDaily"]
   end
 end
