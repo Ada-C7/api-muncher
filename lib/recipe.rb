71173d883dba547fc9ac0c30fdd974df92b376cb
Janice Lichtman <jalichtman@gmail.com>
2017-05-03 20:17:16 -0700

    have show page retriving recipe via ID taken from it's uri

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -5,7 +5,7 @@ class Recipe
 
   BASE_URL = "https://api.edamam.com/search"
 
-  attr_reader :name, :image, :original_url, :uri_identifier
+  attr_reader :name, :image, :id, :original_url
 
   def initialize(recipe_params)
     @name = recipe_params[:name]
@@ -23,45 +23,49 @@ class Recipe
 
     recipes = HTTParty.get(BASE_URL, query: query_params).parsed_response["hits"]
 
+
+
     recipe_array =[]
+
     recipes.each do |recipe|
-      recipe_params = {name: recipe["recipe"]["label"], image: recipe["recipe"]["image"], id: recipe["recipe"]["uri"],
-     original_url: recipe["recipe"]["url"]}
+      id = recipe["recipe"]["uri"].split("_").last
+      
+      recipe_params = {name: recipe["recipe"]["label"], image: recipe["recipe"]["image"], id: id,
+        original_url: recipe["recipe"]["url"]}
 
-      recipe_array << Recipe.new(recipe_params)
+        recipe_array << Recipe.new(recipe_params)
+      end
+      return recipe_array
     end
-    return recipe_array
-  end
 
 
-  def self.getRecipe(name)
+    def self.getRecipe(name)
 
-    query_params = {
-      "app_id" => ENV["EDAMAM_API_APP_ID"],
-      "app_key" => ENV["EDAMAM_API_APP_KEY"],
-      "q" => name
-    }
+      query_params = {
+        "app_id" => ENV["EDAMAM_API_APP_ID"],
+        "app_key" => ENV["EDAMAM_API_APP_KEY"],
+        "q" => name
+      }
 
-    recipe = HTTParty.get(BASE_URL, query: query_params).parsed_response["hits"][0]
+      recipe = HTTParty.get(BASE_URL, query: query_params).parsed_response["hits"][0]
 
+      id = recipe["recipe"]["uri"].split("_").last
 
-    id = id: recipe["recipe"]["uri"]
+      recipe_params = {name: recipe["recipe"]["label"], image: recipe["recipe"]["image"], id: id,
+        original_url: recipe["recipe"]["url"]}
 
-    recipe_params = {name: recipe["recipe"]["label"], image: recipe["recipe"]["image"], id: recipe["recipe"]["uri"],
-   original_url: recipe["recipe"]["url"]}
-
-    return Recipe.new(recipe_params)
-
-
-    # if response["channel"]
-    #   return Channel.new(response["channel"]["name"], response["channel"]["id"])
-    # else
-    #   return nil
-    # end
-  end
+        return Recipe.new(recipe_params)
 
 
-end
+        # if response["channel"]
+        #   return Channel.new(response["channel"]["name"], response["channel"]["id"])
+        # else
+        #   return nil
+        # end
+      end
+
+
+    end
 
 
 
@@ -71,5 +75,5 @@ end
 
 
 
-#return recipe_name = response.parsed_response["hits"][0]["recipe"]["label"]
-#recipe_image = response.parsed_response["hits"][0]["recipe"]["image"]
+    #return recipe_name = response.parsed_response["hits"][0]["recipe"]["label"]
+    #recipe_image = response.parsed_response["hits"][0]["recipe"]["image"]
