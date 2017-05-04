43868114705ba6b1307743be8039410bc05d8afe
Ashtn <ashtonehorton@gmail.com>
2017-05-03 22:16:50 -0700

    added search bar to results and show page, added nav bar

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,15 +3,12 @@ class RecipesController < ApplicationController
   def results
     #raise
     @recipes = EdamamApiWrapper.findRecipes(params[:search])
-
   end
 
   def show
     @recipe = EdamamApiWrapper.showRecipe(params[:uri])
   end
 
-  def search
-    #@recipes = EdamamApiWrapper.findRecipes(params[:search])
-  end
+  def search;end
 
 end
