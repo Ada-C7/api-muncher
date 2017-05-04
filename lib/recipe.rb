b99d482d2f6a904f2466bf86a4db96e3015bad1f
Kayla <kubicke.kayla@gmail.com>
2017-05-04 14:40:03 -0700

    User can view first ten search results on index page.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,17 +1,41 @@
+require 'json'
+
 class Recipe
 
+  attr_reader :label, :image, :url, :ingredient_lines, :diet_labels
 
-  def initialize
-
+  def initialize(label, image, url, ingredient_lines, diet_labels)
+    @label = label
+    @image = image
+    @url = url
+    @ingredient_lines = ingredient_lines
+    @diet_labels = diet_labels
   end
 
   def self.search(search_term)
-    #Basic functionality for method
-    url = "https://api.edamam.com/search?app_key=#{ENV['APP_KEY']}&app_id=#{ENV['APP_ID']}&q=#{search_term}"
+    ##NOTE: HARD CODE NUM OF RETURNS ON END (TO=)
+    url = "https://api.edamam.com/search?app_key=#{ENV['APP_KEY']}&app_id=#{ENV['APP_ID']}&q=#{search_term}&to=10"
 
     response = HTTParty.get(url).parsed_response
 
-    return response
+    recipes_list = []
+
+    # Working, but loop needs to be DRYed
+    count = 0
+    10.times do
+      label = response['hits'][count]['recipe']['label']
+      image = response['hits'][count]['recipe']['image']
+      url = response['hits'][count]['recipe']['url']
+      ingredient_lines = response['hits'][count]['recipe']['ingredientLines']
+      diet_labels = response['hits'][count]['recipe']['dietLabels']
+
+      recipes_list << Recipe.new(label, image, url, ingredient_lines, diet_labels)
+      count += 1
+    end
+
+    # raise
+
+    return recipes_list#recipe_display
   end
 
   def list_display
