4f9fdd18e72a4d93ff4326f259f226870ceef8c5
EricaJCase <ericajcase@gmail.com>
2017-05-04 07:43:02 -0700

    index view complete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,13 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = EdamamApiWrapper.all
+    @recipes = EdamamApiWrapper.all("tacos")
   end
 
   def show
   end
 
   def new
-
+    @recipe = Recipe.new
   end
+
 end
