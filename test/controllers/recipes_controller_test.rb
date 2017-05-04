2a169a7dcc791c60b5465276427b3c0578b7c9d6
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-04 16:15:57 -0700

    fixed recipes controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,17 +2,27 @@ require "test_helper"
 
 describe RecipesController do
   describe 'index' do
+    before do
+      @params = {
+                  search_text: "cookies",
+                  from: "0"
+                }
+      @no_results = {
+                      search_text: "cookies",
+                      from: "0"
+                    }
+    end
 
     it 'returns the index page/search results with results' do
       VCR.use_cassette("search_results") do
-        get recipes_path("cookies")
+        get recipes_path, params: @params
         must_respond_with :success
       end
     end
 
     it 'returns the index page/search results with no results' do
       VCR.use_cassette("search_results") do
-        get recipes_path("dlfjakldfjakldfjakl")
+        get recipes_path, params: @no_results
         must_respond_with :success
       end
     end
