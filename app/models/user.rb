7abbb557b59db3c0dd8f8e205974dac065e6705a
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-04 17:51:10 -0700

    user model tests written and passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :searches
 
-  validates :provider, presence: true, uniqueness: true
+  validates :provider, presence: true
   validates :uid, presence: true, uniqueness: true
 
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,32 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  it "must be valid with provider and uid" do
+    user = User.create(provider: "provider", uid: "uid")
+    user.must_be_instance_of User
+
+  end
+
+  it "must fail without uid" do
+      user = User.new(provider: "provider")
+      user.save.must_equal false
+  end
+
+  it "must fail without provider" do
+    user = User.new(uid: "uid")
+    user.save.must_equal false
+  end
+
+  it "uid must be unique" do
+    user1 = User.create(provider: "provider", uid: "1")
+    user2 = User.new(provider: "provider", uid: "1")
+    user2.save.must_equal false
+  end
+
+  it "must have attributes" do
+    user = User.create(provider: "provider", uid: "uid")
+    user.provider.must_equal "provider"
+    user.uid.must_equal "uid"
   end
 end
