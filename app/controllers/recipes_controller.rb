5a905cf494e060f44541ac04aa9f4171f6172104
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-04 18:10:32 -0700

    Addded well_paginate gem

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,8 @@ class RecipesController < ApplicationController
   def index
     if params[:q]
       @recipes = SearchApiWrapper.listRecipes(params[:q])
+      @recipes_pages = @recipes.paginate(:page => params[:page], :per_page => 10)
+
     # @recipes = SearchApiWrapper.getRecipes("chicken")
     else
       @recipes = []
