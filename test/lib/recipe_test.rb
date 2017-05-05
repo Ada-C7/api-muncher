d13499b7dcd2c86f87a14202e9cb67c3b8fda282
Haby Randall <sow.haby@gmail.com>
2017-05-05 01:15:00 -0700

    Adding vcr and webmock to gemfile

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,1 +1,23 @@
 require 'test_helper'
+
+describe Recipe do
+  it "Cannot be initialized without parameters" do
+    proc {
+      Recipe.new
+    }.must_raise ArgumentError
+
+    proc {
+      Recipe.new "Name"
+    }.must_raise ArgumentError
+  end
+
+  it "Must initialize name and id properly" do
+
+  end
+
+  it "Can create a recipe instance" do
+
+  end
+
+
+end
