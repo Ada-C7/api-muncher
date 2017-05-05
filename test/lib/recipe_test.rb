7361f065034a283007d0f0d4dd1778d96326b0aa
Cara Comfort <cara.comfort10@gmail.com>
2017-05-05 15:58:37 -0700

    Add header with logo

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 describe Recipe do
   describe "search" do
-    it "Can find recipe(s) with a valid search" do
+    it "Returns an array of recipe objects for a valid search" do
       VCR.use_cassette("recipes") do
         recipes = Recipe.search("q" => "brownies")
         recipes.count.must_be :>, 0
@@ -22,15 +22,12 @@ describe Recipe do
 
     it "Raises an error when given invalid search parameters" do
       VCR.use_cassette("recipes") do
-        # recipes = Recipe.search("fake")
-        # recipes.must_equal 404
         proc {
           Recipe.search("from" => nil, "to" => nil, "q" => nil)
         }.must_raise Recipe::RecipeError
       end
     end
 
-    #returns an empty array when no recieps are found
 
     #finds
   end
