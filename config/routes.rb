dd7f1048180518d20473b796dd9b5993cd153e3b
amb54 <ambarklund@hotmail.com>
2017-05-05 11:08:05 -0700

    Updated routes for root, post /recipes and get /recipes/:uri.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+root 'recipes#root'
+
 get '/recipes', to: 'recipes#index', as: 'recipes'
-get '/recipes/:recipe/show', to: 'recipes#show', as: 'recipe'
-get '/recipes/:search_text', to: 'recipe#search', as: 'search'
+post '/recipes', to: 'recipes#index'
+get '/recipes/:uri', to: 'recipes#show', as: 'recipe'
+# get '/recipes/show', to: 'recipes#show', as: 'recipe'
 
 end
