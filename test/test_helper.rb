ca05f4243cad0c900b6f72b6674f1b5a66197c40
Kayla <kubicke.kayla@gmail.com>
2017-05-04 17:55:12 -0700

    Setup for VCR.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -30,7 +30,7 @@ end
 
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'test/recipexs'
+  config.cassette_library_dir = 'test/recipes'
   config.hook_into :webmock
   config.default_cassette_options = {
     :record => :new_episodes,
