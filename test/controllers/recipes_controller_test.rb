1637c758a88ccf83512da0ad2d246122f49271ee
Sofia Kim <sofiamhkim@gmail.com>
2017-05-05 10:59:10 -0700

    library test fixed

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -3,8 +3,10 @@ require "test_helper"
 describe RecipesController do
   describe "index" do
     it "succeeds with serach terms" do
-      get index_path(:q => "o")
-      must_respond_with :success
+      VCR.use_cassette("recipes") do
+        get index_path(:q => "o")
+        must_respond_with :success
+      end
     end
   end
 end
