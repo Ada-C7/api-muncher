b9560fa8e2cc91db2f4f11854e2e041727f79ada
Haby Randall <sow.haby@gmail.com>
2017-05-05 01:01:00 -0700

    Setting up VCR for testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,15 +2,17 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
-    require "minitest/reporters"  # for Colorized output
+require 'vcr'
+require 'webmock/minitest'
+require "minitest/reporters"  # for Colorized output
 
-    #  For colorful output!
-    Minitest::Reporters.use!(
-    Minitest::Reporters::SpecReporter.new,
-    ENV,
-    Minitest.backtrace_filter
-    )
-
+#  For colorful output!
+# Minitest::Reporters.use!(
+# Minitest::Reporters::SpecReporter.new,
+# ENV,
+# Minitest.backtrace_filter
+# )
+Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new, Minitest::Reporters::DefaultReporter.new(:color => true)]
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
@@ -20,6 +22,18 @@ require "minitest/rails"
 # require "minitest/pride"
 
 class ActiveSupport::TestCase
+  #setting up VCR
+  VCR.configure do |config|
+    config.cassette_library_dir = "test/cassettes"
+    config.hook_into :webmock
+    config.default_cassette_options = {
+      record: :new_episodes,
+      match_requests_on: [:method, :uri, :body]
+    }
+    config.filter_sensitive_data("<EDAMAM_APP_ID>") { ENV["EDAMAM_APP_ID"] }
+    config.filter_sensitive_data("<EDAMAM_APP_KEY>") { ENV["EDAMAM_APP_KEY"] }
+  end
+
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
