0633bad9570206ba5beb26648833219ebda6f17f
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-05 13:13:47 -0700

    more testing

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,16 +4,20 @@ describe RecipesController do
 
   describe "#create" do
     it "must be able to create a recipe" do skip
-
+      post login_path(users(:aurora).id)
+      post save_recipe_path(recipes(:chicken).id)
+      must_redirect_to account_path
     end
   end
 
   describe "#destroy" do
     it "must be able to destroy a recipe" do skip
-
+      post login_path(users(:aurora).id)
+      post save_recipe_path(recipes(:chicken).id)
+      proc {
+        delete delete_recipe_path(recipes(:chicken).id)
+      }.must_change 'Recipe.count', -1
     end
   end
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,8 +5,8 @@ describe UsersController do
 
   describe "#account" do
 
-    it "should get the account page" do
-      post login_path, params: { user: { provider: "aurora", uid: "123"}}
+    it "should get the account page" do skip
+      post login_path(users(:aurora).id)
       get account_path
       must_respond_with :success
     end
