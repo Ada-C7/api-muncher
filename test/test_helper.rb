21f43014244060853f619313cfd97e741a9db0a7
Kayla <kubicke.kayla@gmail.com>
2017-05-04 17:52:08 -0700

    Setup for VCR.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
+require 'vcr'
+require 'webmock/minitest'
 
 #  For colorful output!
 Minitest::Reporters.use!(
@@ -23,4 +25,23 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+end
+
+
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/recipexs'
+  config.hook_into :webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:method, :uri, :body]
+  }
+
+  config.filter_sensitive_data("<EAT_KEY>") do
+    ENV['APP_KEY']
+  end
+
+  config.filter_sensitive_data("<EAT_ID>") do
+    ENV['APP_ID']
+  end
 end
