7e5b78be5593f31d4f7dd863a70d2536622f1ce5
Anacelia Gillette <acgillette426@gmail.com>
2017-05-05 14:17:11 -0700

    Added more api wrapper tests

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe EdamamApiWrapper do
 
-  it "It gets a list of recipes in an array" do
+  it "It returns a list of recipes in an array" do
     VCR.use_cassette("recipes") do
       search = "chicken"
       response = EdamamApiWrapper.findRecipes(search)
@@ -18,11 +18,38 @@ describe EdamamApiWrapper do
     end
   end
 
-  it "Does not get a list without a search parameter" do
+  it "Does not return a list without a search parameter" do
     proc {
     VCR.use_cassette("recipes") do
       response = EdamamApiWrapper.findRecipes()
     end
     }.must_raise ArgumentError
+  end
+
+  it "returns a list of nine items" do
+    VCR.use_cassette("recipes") do
+      search = "chicken"
+      response = EdamamApiWrapper.findRecipes(search)
+      response.length.must_equal 9
+    end
+  end
+
+  it "returns a list starting from a certain point" do
+    VCR.use_cassette("recipes") do
+      search = "chicken"
+      response1 = EdamamApiWrapper.findRecipes(search)
+      response2 = EdamamApiWrapper.findRecipes(search, 10, 19)
+      response1[0].label.wont_equal response2[0].label
+    end
+  end
+
+  it "returns a single recipe" do
+    VCR.use_cassette("recipe") do
+      search = "chicken"
+      response1 = EdamamApiWrapper.findRecipes(search)[0]
+
+      response2 = EdamamApiWrapper.findRecipe(response1.uri)
+      response2.class.must_equal Recipe
+    end
   end
 end
