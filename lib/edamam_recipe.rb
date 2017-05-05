9dddfca3c965ac0e9a4932bdf49b71c4517ec2e4
amb54 <ambarklund@hotmail.com>
2017-05-05 11:12:20 -0700

    Updated lib/edamam_recipe.rb. Added contstant for the path to a specific recipe. Corrected misstake in initializer. Coomented out and moved code that is currently not used. Updated self.search to accept three parameters. Updated self.show.

--- lib/edamam_recipe.rb
+++ lib/edamam_recipe.rb
@@ -1,20 +1,23 @@
+# require 'httparty'
+
 class EdamamRecipe
 
   class EdamamException < StandardError
   end
 
   BASE_URL_SEARCH = "https://api.edamam.com/search"
+  BASE_URI_SEARCH = "http://www.edamam.com/ontologies/edamam.owl%23"
 
   attr_reader :uri, :label, :image
 
   def initialize(info_hash)
     @uri = info_hash[:uri]
-    @label = nfo_hash[:label]
-    @image = nfo_hash[:image]
+    @label = info_hash[:label]
+    @image = info_hash[:image]
   end
 
-  def self.search(search_text)
-    url = "#{BASE_URL_SEARCH}?q=#{search_text}&api_id=#{ENV["EDAMAM_APP_ID"]}&api_key=#{ENV[EDAMAM_APP_KEY]}"
+  def self.search(search_text, from = "0", to = "10")
+    url = "#{BASE_URL_SEARCH}?q=#{search_text}&from=#{from}&to=#{to}&api_id=#{ENV["EDAMAM_APP_ID"]}&api_key=#{ENV["EDAMAM_APP_KEY"]}"
     response = HTTParty.get(url)
     recipes_array = []
     response["hits"].each do |recipe|
@@ -24,23 +27,34 @@ class EdamamRecipe
         recipes_array << self.find(recipe["recipe"]["uri"])
       end
     end
-    return recipes_array
+    return [recipes_array, response["count"]]
   end
 
-  def self.all
-    ObjectSpace.each_object(self).to_a
-  end
-
-  def self.find(uri)
-    found_uri = self.all.select { |object| object.uri == uri}
-    return found_uri.first # returns nil if for empty array
-  end
-
-  def show
-    uri = @uri.gsub("#","%23")
-    url = "#{BASE_URL_SEARCH}?r=#{uri}&api_id=#{ENV["EDAMAM_APP_ID"]}&api_key=#{ENV[EDAMAM_APP_KEY]}"
+  def self.show(uri)
+    url = "#{BASE_URL_SEARCH}?r=#{BASE_URI_SEARCH}#{uri}&api_id=#{ENV["EDAMAM_APP_ID"]}&api_key=#{ENV["EDAMAM_APP_KEY"]}"
     response = HTTParty.get(url)
     return response
   end
 
 end # END of class EdamamRecipe
+
+
+# test1 = EdamamRecipe.new(uri: "uri", label: "this is the label", image: "soemthing")
+# puts test1
+
+# puts EdamamRecipe.new(uri: "uri_aha", label: "this is the label", image: "soemthing").uri
+
+
+# def self.find(uri)
+#   found_uri = self.all.select { |object| object.uri == uri}
+#   return found_uri.first # returns nil if for empty array
+# end
+#
+# def self.find(obj)
+#   found_obj = self.all.select { |object| object == obj}
+#   return found_obj.first # returns nil if for empty array
+# end
+
+# def self.all
+#   ObjectSpace.each_object(self).to_a
+# end
