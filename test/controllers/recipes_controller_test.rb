2a83646c7bb8b1592ca4d7e05b8e39938eea3cbe
Anacelia Gillette <acgillette426@gmail.com>
2017-05-05 14:55:28 -0700

    Added a couple new images to background on site

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,8 +2,13 @@ require "test_helper"
 
 describe RecipesController do
   it "should get index" do
-    get recipes_index_url
-    value(response).must_be :success?
+    get root_path
+    must_respond_with :success
+  end
+
+  it "should get a list of recipes" do
+    get recipes_path, params: { search: "chicken" }
+    must_respond_with :success
   end
 
 end
