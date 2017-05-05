5934d101403c2f3bd82f5b5c7e4933c8f98d2079
Janice Lichtman <jalichtman@gmail.com>
2017-05-05 04:28:17 -0700

    pagination working except for viewing the last fiew recipies

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -28,7 +28,8 @@ class Recipe
       "app_key" => ENV["EDAMAM_API_APP_KEY"],
       "q" => item,
       "from" => from,
-      "to" => to
+      "to" => to,
+      "calories" => "gte20,lte28"
     }
 
     recipes = HTTParty.get(BASE_URL, query: query_params).parsed_response
