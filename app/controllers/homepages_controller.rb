98ae45bb4a6873d26eecbc6efc6910dbfa0feb5c
Spatterjaaay <aja.pursova@gmail.com>
2017-05-04 22:14:25 -0700

    added logic for displaying pages of ten recipes and added next, previous and home buttons to the views

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -2,11 +2,35 @@ class HomepagesController < ApplicationController
   def index;end
 
   def list
-    @recipes = EdamamApiWrapper.listRecipes(params[:search_term])
-    #if params[:food]
-    # else
-    #   flash[:warning] = "please enter search term"
-    #   redirect_to :root
+    if params[:search_term] && params[:search_term] != ""
+      @recipes = EdamamApiWrapper.listRecipes(params[:search_term], params[:page])
+    else
+      flash[:warning] = "please enter search term"
+      redirect_to :root
+      return
+    end
+
+    @search_term = params[:search_term]
+
+    if !params[:page]
+      page = 1
+    else
+      page = params[:page].to_i
+    end
+
+    if page == 1
+      @previous_link = ""
+    else
+      @previous_link = "<a class=\"button\" href=\"" + list_recipes_path(:search_term => params[:search_term], :page => page - 1) + "\">Previous</a>"
+    end
+
+    if @recipes.length < 10
+      @next_link = ""
+    else
+      @next_link = "<a class=\"button\" href=\"" + list_recipes_path(:search_term => params[:search_term], :page => page + 1) + "\">Next</a>"
+    end
+
+
   end
 
   def show
--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,25 +1,28 @@
 require 'httparty'
+require 'will_paginate/array'
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search?"
   APP_KEY = ENV["EDAMAM_TOKEN"]
   APP_ID = ENV["APP_TOKEN"]
 
-  def self.listRecipes(search_term)
+  def self.listRecipes(search_term, page)
 
     search = URI.encode(search_term)
     url = BASE_URL + "q=#{search}&" + "app_id=#{APP_ID}&" + "app_key=#{APP_KEY}"
+    if page
+      from_recipe = page.to_i * 10 - 10
+      url += "&from=#{from_recipe}"
+    end
 
     response = HTTParty.get(url)
     search_results = response["hits"]
-
     recipes = []
     if search_results
         search_results.each do |recipe|
         recipes << Recipe.new(recipe["recipe"]["label"], recipe["recipe"]["image"], recipe["recipe"]["uri"].partition("recipe_").last, recipe["recipe"]["url"])
       end
     end
-
     return recipes
   end
 
