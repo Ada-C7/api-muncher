c2797dd62725848c185c847c1577ce4e37d4a2fb
amb54 <ambarklund@hotmail.com>
2017-05-05 11:19:18 -0700

    Updated the action index and show for the recipies controller.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,18 +1,24 @@
 require 'edamam_recipe'
 
 class RecipesController < ApplicationController
+  def root
+  end
 
   def index
-
+    @recipes = EdamamRecipe.search(params[:search_text], params[:from], params[:to])
+    @text = params[:search_text]
+    @from = params[:from]
+    @to = params[:to]
+      
   end
 
   def show
+    # @recipe = EdamamRecipe.show("http://www.edamam.com/ontologies/edamam.owl#recipe_f1c853a77986214680bbdd424883499a")
+    @recipe = EdamamRecipe.show(params[:uri])
+  end
+
+  def more
 
   end
 
-  def search
-
-  end
-
-
-end # END of class RecipesController 
+end # END of class RecipesController
