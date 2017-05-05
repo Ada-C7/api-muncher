c61bf66546b87720d559410e3569821ac80b6c1d
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-05 11:45:44 -0700

    omg, I have a header and search bar. woo hoo

---
+++ app/controllers/searches_controller.rb
@@ -0,0 +1,11 @@
+require "edamam"
+
+class SearchesController < ApplicationController
+  def index #all_search_results
+
+  end
+
+  def show #one_result
+
+  end
+end
--- lib/edamam.rb
+++ lib/edamam.rb
@@ -1,5 +1,17 @@
 class Edamam
   class Edamem < StandardError; end
 
-  
+  BASE_URL = "https://api.edamam.com/search?"
+
+  def search(search_item)
+    query_params = {
+      "q" => search_item,
+      "id" => ENV["edamam_id"],
+      "key" => ENV["edamam_key"]
+    }
+    response = HTTParty.post(BASE_URL, query: query_params).parsed_response
+
+    response["hits"]
+
+  end
 end
