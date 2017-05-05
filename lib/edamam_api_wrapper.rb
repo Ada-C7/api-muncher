7094934fe50baa149ff79123233bb812931c8c0a
Spatterjaaay <aja.pursova@gmail.com>
2017-05-05 13:34:06 -0700

    fixed tests fot listing recipes wrapper method

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,5 +1,4 @@
 require 'httparty'
-require 'will_paginate/array'
 
 class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search?"
--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -1,23 +1,24 @@
 require 'test_helper'
 
 describe EdamamApiWrapper do
-  describe "self.listRecipes(search_term)" do
-    it "Can get a list of recipes" do
+  describe "self.listRecipes(search_term, page)" do
+    it "Gets a list of recipes" do
       VCR.use_cassette("recipe") do
-        recipes = EdamamApiWrapper.listRecipes("carrot")
+        recipes = EdamamApiWrapper.listRecipes("carrot", "1")
 
         recipes.must_be_instance_of Array
         recipes.each do |recipe|
           recipe.must_be_instance_of Recipe
         end
+        recipes.first.name.must_equal "Carrots Braised in Beer and Carrot Juice"
       end
     end
     # this method has no paramters, if it had, we could test what would happen
     # it we gave it a broken parameter
     # if we pass it token as a prameter, the only way for it to fail is to have invalid token
-    it "will return an empty array with a bad parameter" do
+    it "Returns an empty array with a bad parameter" do
       VCR.use_cassette("recipe") do
-        channels = EdamamApiWrapper.listRecipes("stupid")
+        channels = EdamamApiWrapper.listRecipes("stupid", 0)
         channels.must_equal []
       end
     end
