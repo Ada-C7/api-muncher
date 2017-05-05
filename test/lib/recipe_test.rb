b36b60fa80fb1de551d6e2f68085af8b98e8b297
Kayla <kubicke.kayla@gmail.com>
2017-05-04 17:01:59 -0700

    DRYed up initialize tests and refined some test descriptions.

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -2,60 +2,46 @@ require 'test_helper'
 
 describe Recipe do
   describe 'initialize' do
-    it 'recipe is created with 5 attributes' do
-      # Attributes: label, image, url, ingredient list,
-      # and diet label
-      recipe = Recipe.new(
+    before do
+      @recipe = Recipe.new(
       'Rotten Strawberry Stew',
-      'https://www.rottenstrawberriesmakemesad.com/image.jpg',
-      'https://www.gettheserottenstrawberriesoutoutofmyfridge.com/terriblerecipe',
+      'https://www.rottenstrawberries.com/image.jpg',
+      'https://www.rottenstrawberries.com/terriblerecipe',
       ['rotten strawberries', 'sadness', 'shame'],
       ['Do not eat', 'why are you eating these?']
       )
+    end
 
-      recipe.label.must_equal 'Rotten Strawberry Stew'
-      recipe.image.must_equal 'https://www.rottenstrawberriesmakemesad.com/image.jpg'
-      recipe.url.must_equal 'https://www.gettheserottenstrawberriesoutoutofmyfridge.com/terriblerecipe'
-      recipe.ingredient_lines.must_equal ['rotten strawberries', 'sadness', 'shame']
-      recipe.diet_labels.must_equal ['Do not eat', 'why are you eating these?']
+    it 'recipe must have all 5 attributes' do
+      @recipe.label.must_equal 'Rotten Strawberry Stew'
+      @recipe.image.must_equal 'https://www.rottenstrawberries.com/image.jpg'
+      @recipe.url.must_equal 'https://www.rottenstrawberries.com/terriblerecipe'
+      @recipe.ingredient_lines.must_equal ['rotten strawberries', 'sadness', 'shame']
+      @recipe.diet_labels.must_equal ['Do not eat', 'why are you eating these?']
     end
     # Think about arrays vs. strings
 
-    it 'recipe can be created with empty string attribute' do
-      recipe = Recipe.new(
-      'Rotten Strawberry Stew',
-      '',
-      'https://www.gettheserottenstrawberriesoutoutofmyfridge.com/terriblerecipe',
-      ['rotten strawberries', 'sadness', 'shame'],
-      ['Do not eat', 'why are you eating these?']
-      )
+    it 'recipe can have an empty string attribute' do
+      @recipe.instance_variable_set(:@image, '')
 
-      recipe.label.must_equal 'Rotten Strawberry Stew'
-      recipe.image.must_equal ''
-      recipe.url.must_equal 'https://www.gettheserottenstrawberriesoutoutofmyfridge.com/terriblerecipe'
-      recipe.ingredient_lines.must_equal ['rotten strawberries', 'sadness', 'shame']
-      recipe.diet_labels.must_equal ['Do not eat', 'why are you eating these?']
+      @recipe.label.must_equal 'Rotten Strawberry Stew'
+      @recipe.image.must_equal ''
+      @recipe.url.must_equal 'https://www.rottenstrawberries.com/terriblerecipe'
+      @recipe.ingredient_lines.must_equal ['rotten strawberries', 'sadness', 'shame']
+      @recipe.diet_labels.must_equal ['Do not eat', 'why are you eating these?']
     end
 
-    it 'recipe is created with an empty array' do
-      # Attributes: label, image, url, ingredient list,
-      # and diet label
-      recipe = Recipe.new(
-      'Rotten Strawberry Stew',
-      'https://www.rottenstrawberriesmakemesad.com/image.jpg',
-      'https://www.gettheserottenstrawberriesoutoutofmyfridge.com/terriblerecipe',
-      ['rotten strawberries', 'sadness', 'shame'],
-      []
-      )
+    it 'recipe can have an empty array attribute' do
+      @recipe.instance_variable_set(:@diet_labels, [])
 
-      recipe.label.must_equal 'Rotten Strawberry Stew'
-      recipe.image.must_equal 'https://www.rottenstrawberriesmakemesad.com/image.jpg'
-      recipe.url.must_equal 'https://www.gettheserottenstrawberriesoutoutofmyfridge.com/terriblerecipe'
-      recipe.ingredient_lines.must_equal ['rotten strawberries', 'sadness', 'shame']
-      recipe.diet_labels.must_equal []
+      @recipe.label.must_equal 'Rotten Strawberry Stew'
+      @recipe.image.must_equal 'https://www.rottenstrawberries.com/image.jpg'
+      @recipe.url.must_equal 'https://www.rottenstrawberries.com/terriblerecipe'
+      @recipe.ingredient_lines.must_equal ['rotten strawberries', 'sadness', 'shame']
+      @recipe.diet_labels.must_equal []
     end
 
-    it 'recipe is not created without all 5 attributes' do
+    it 'recipe must have 5 attributes' do
       proc { Recipe.new }.must_raise ArgumentError
     end
 
