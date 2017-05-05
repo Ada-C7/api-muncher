52cc94167e7343a138d1ad3f92fce342206b47f5
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-05-05 12:02:52 -0700

    changed some auth things

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,18 @@
 class User < ApplicationRecord
   def self.from_omniauth(auth)
-   where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-     user.provider = auth.provider
-     user.uid = auth.uid
-     user.name = auth.info.name
-     user.oauth_token = auth.credentials.token
-     user.oauth_expires_at = Time.at(auth.credentials.expires_at)
-     user.save!
-   end
+    user = User.new
+    user.name = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+    user.oauth_id = auth_hash["uid"]
+    user.oauth_provider = "github"
+    return user
+  #  where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+  #    user.provider = auth.provider
+  #    user.uid = auth.uid
+  #    user.name = auth.info.name
+  #    user.oauth_token = auth.credentials.token
+  #    user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+  #    user.save!
+  #  end
  end
 end
