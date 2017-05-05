6b5f120f679b715b9b76c9a1947e9adf87fc20b5
Haby Randall <sow.haby@gmail.com>
2017-05-04 19:07:47 -0700

    Trying out paginate gem

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,11 @@
+
+
 class RecipesController < ApplicationController
 
   def index
     if params[:q]
-      @recipes = EdamamApiWrapper.search(params[:q])
+      @recipes = EdamamApiWrapper.search(params[:q]).paginate(:page => params[:page], :per_page => 10)
+
     end
   end
 
