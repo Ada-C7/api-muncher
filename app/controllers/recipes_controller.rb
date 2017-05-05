76f16f8fd29d67104f574d1b39a76e28d3b04902
Cynthia Cobb <cobbcynthiad@gmail.com>
2017-05-05 12:43:13 -0700

    working on homepage styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,15 +2,17 @@ require 'edamam_search'
 class RecipesController < ApplicationController
 
   def index
-    search_params = {
-                      search_text: params[:search_text],
-                      from: params[:from].to_i,
-                      to: params[:from].to_i + 10
-                    }
-    raise
-    @new_search = EdamamSearch.new(search_params)
-    search_results = @new_search.search_results
-    @list_of_recipes = Recipe.list_of_recipes(search_results)
+    if params[:search_text]
+      search_params = {
+                        search_text: params[:search_text],
+                        from: params[:from].to_i,
+                        to: params[:from].to_i + 10
+                      }
+      raise
+      @new_search = EdamamSearch.new(search_params)
+      search_results = @new_search.search_results
+      @list_of_recipes = Recipe.list_of_recipes(search_results)
+    end
   end
 
   # will show results from search
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  root 'recipes#index'
   get 'search', to: 'recipes#search', as: 'recipe_search'
   # get 'recipe/:id', to: 'recipes#show', as: 'recipe'
   resources :recipes, only: [:index, :show]
