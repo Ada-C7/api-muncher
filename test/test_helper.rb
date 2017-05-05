d877878fdbc259d3b6d8d68347bf5dee36938899
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-05-04 19:43:03 -0700

    commenting out simplecov stuff as it is breaking suddenly

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,6 @@
+# require 'simplecov'
+# SimpleCov.start
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
@@ -6,15 +9,35 @@ require "minitest/reporters"  # for Colorized output
 require 'vcr'
 require 'webmock/minitest'
 
+
 #  For colorful output!
 Minitest::Reporters.use!(
-Minitest::Reporters::SpecReporter.new,
-ENV,
-Minitest.backtrace_filter
+  Minitest::Reporters::SpecReporter.new,
+  ENV,
+  Minitest.backtrace_filter
 )
 
-require 'simplecov'
-SimpleCov.start
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  #if no folder found, it will be created
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Slack token lying around in a cassette file.
+  config.filter_sensitive_data("<EDAMAM_ID>") do
+    ENV['EDAMAM_ID']
+  end
+    config.filter_sensitive_data("<EDAMAM_KEY>") do
+    ENV['EDAMAM_KEY']
+  end
+end
+
+Minitest::Reporters.use!
+[Minitest::Reporters::SpecReporter.new, Minitest::Reporters::JUnitReporter.new, ENV,
+ Minitest.backtrace_filter]
+
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
@@ -23,25 +46,7 @@ SimpleCov.start
 # require "minitest/pride"
 
 class ActiveSupport::TestCase
-
-  class ActiveSupport::TestCase
-
-    VCR.configure do |config|
-      config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
-      config.hook_into :webmock # tie into this other tool called webmock
-      config.default_cassette_options = {
-        :record => :new_episodes,    # record new data when we don't have it yet
-        :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
-      }
-      # Don't leave our Slack token lying around in a cassette file.
-      config.filter_sensitive_data("<EDAMAM_ID>", "<EDAMAM_KEY>") do
-        ENV['EDAMAM_ID']
-        ENV['EDAMAM_KEY']
-      end
-
-    end
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
-    # Add more helper methods to be used by all tests here...
-  end
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+  # Add more helper methods to be used by all tests here...
 end
