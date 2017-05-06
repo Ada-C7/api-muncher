7bc66c72f50d2a545a695efafc911b0a222e6acb
Spatterjaaay <aja.pursova@gmail.com>
2017-05-06 15:14:55 -0700

    added stuling for previous and next button, and search field

--- app/helpers/homepages_helper.rb
+++ app/helpers/homepages_helper.rb
@@ -7,9 +7,9 @@ module HomepagesHelper
     end
 
     if page == 1
-      @previous_link = ""
+      @previous_link =   "<li class=\"pagination-previous disabled\">Previous</li>"
     else
-      @previous_link = "<a class=\"button\" href=\"" + list_recipes_path(:search_term => params[:search_term], :page => page - 1) + "\">Previous</a>"
+      @previous_link = "<li class=\"pagination-previous\"><a href=\"" + list_recipes_path(:search_term => params[:search_term], :page => page - 1) + "\" aria-label=\"Previous page\">Previous</a></li>"
     end
   end
 
@@ -21,9 +21,9 @@ module HomepagesHelper
     end
 
     if @recipes.length < 10
-      @next_link = ""
+      @next_link = "<li class=\"pagination-next disabled\">Next</li>"
     else
-      @next_link = "<a class=\"button\" href=\"" + list_recipes_path(:search_term => params[:search_term], :page => page + 1) + "\">Next</a>"
+      @next_link = "<li class=\"pagination-next\"><a href=\"" + list_recipes_path(:search_term => params[:search_term], :page => page + 1) + "\" aria-label=\"Next page\">Next</a></li>"
     end
   end
 end
