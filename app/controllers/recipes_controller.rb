cfa36d1d512df95750f11e80fe48d4c5d0140fcc
Rana Sulaiman <rs_sulaiman@hotmail.com>
2017-05-06 18:39:08 -0700

    Fixed a bug in recipes#index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,20 +3,16 @@ require "search_api_wrapper"
 class RecipesController < ApplicationController
 
   def index
-    # if params[:q]
-      @recipes = SearchApiWrapper.listRecipes(params[:q])
-      @recipes_pages = @recipes.paginate(:page => params[:page], :per_page => 10)
 
-      if @recipes_pages.empty?
-        flash[:error] = "Sorry, there is no match"
-      # else
-      #   return @recipes
+      if params[:q]
+        @recipes = SearchApiWrapper.listRecipes(params[:q])
+        @recipes_pages = @recipes.paginate(:page => params[:page], :per_page => 10)
+
+        if @recipes == []
+          flash[:error] = "Sorry, there is no match"
+          redirect_to root_path
+        end
       end
-
-    # @recipes = SearchApiWrapper.getRecipes("chicken")
-    # else
-      # @recipes = []
-    # end
   end
 
   def show
