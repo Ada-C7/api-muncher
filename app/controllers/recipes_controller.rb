cec416802e78046f71cdfc6162d247362c0debdf
add2point71dots <alcland@gmail.com>
2017-05-07 16:50:21 -0700

    recipe controller tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,10 @@
 class RecipesController < ApplicationController
   def index
-    if params[:search]
-      @recipes = EdamamApiWrapper.list_recipes(params[:search], params[:page].to_i)
-    else
-      @recipes = []
-    end
+    @recipes = EdamamApiWrapper.list_recipes(params[:search], params[:page].to_i)
   end
 
   def show
     @recipe = EdamamApiWrapper.get_recipe(params[:uri])
+    render_404 if !@recipe
   end
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  before do
+    VCR.insert_cassette("edamam")
+  end
+
+  after do
+    VCR.eject_cassette("edamam")
+  end
+
+  it "should get index page with no search" do
+    get recipes_path
+    must_respond_with :success
+  end
+
+  it "should get recipes list with a search term" do
+    get recipes_path, params: { search: "broccoli" }
+    must_respond_with :success
+  end
+
+  it "should get recipes list with search & page params" do
+    get recipes_path, params: {search: "broccoli", page: "2"}
+    must_respond_with :success
+  end
+
+  it "should get individual recipe with valid id" do
+    get recipe_path("938758bcb9dc1de7953ee80525102921")
+    must_respond_with :success
+  end
+
+  it "should redirect to 404 if given a bad recipe id" do
+    get recipe_path("badID")
+    must_respond_with :missing
+  end
 end
