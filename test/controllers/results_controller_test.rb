6409f86ca8048d323ce3b82049e975682255984a
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-07 13:32:30 -0700

    all tests passing

--- test/controllers/results_controller_test.rb
+++ test/controllers/results_controller_test.rb
@@ -2,9 +2,10 @@ require "test_helper"
 
 describe ResultsController do
   it "should get index" do
-    VCR.use_cassette("edmam")
-    get searches_index_path
-    must_respond_with :success
+    VCR.use_cassette("edmam") do
+      get results_path, :q => "beans", :from => 1, :to => 11
+      must_respond_with :success
+    end
   end
 
 end
--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -2,9 +2,9 @@ require "test_helper"
 
 describe SearchesController do
   it "should get index" do
-    VCR.use_cassette("edmam")
-    get searches_index_path
-    must_respond_with :success
+    VCR.use_cassette("edmam") do
+      get root_path
+      must_respond_with :success
+    end
   end
-
 end
