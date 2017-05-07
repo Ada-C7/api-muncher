f172c8d239a0f19df5547382105233033a7596f5
Chantal Rollison <rollison.chantal@gmail.com>
2017-05-07 13:08:27 -0700

    removed duplicate test file

--- test/models/recipe_response_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe RecipeResponse do
-  let(:recipe_response) { RecipeResponse.new }
-
-  it "must be valid" do
-    value(recipe_response).must_be :valid?
-  end
-end
