bf63f704877b1232bde6c21477a96ea29eb54043
Sofia Kim <sofiamhkim@gmail.com>
2017-05-07 16:04:48 -0700

    changed show page table layouts

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,6 +10,7 @@ class RecipesController < ApplicationController
     @list = Recipe.get_recipes(@search_term, params[:from])
   end
 
+  #http://localhost:3000/recipes/index?utf8=%E2%9C%93&q=o&from=0&commit=Search
   def show
     @params = params[:uri] + "." + params[:format].gsub(/#/,'%23')
     @recipe = Recipe.find(@params)
