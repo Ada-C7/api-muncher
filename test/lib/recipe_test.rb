4fc3ba0ca66fbcaf221f846ad5e6e04c56155664
Kayla <kubicke.kayla@gmail.com>
2017-05-07 15:08:17 -0700

    Updated library tests.
    Removed incorrect attribute tests.
    Added edge cases for both methods.

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -3,57 +3,53 @@ require 'recipe'
 
 describe Recipe do
   describe 'initialize' do
-      let(:recipe) { Recipe.new(
-        'Rotten Strawberry Stew',
-        'https://www.rottenstrawberries.com/image.jpg',
-        'http://www.edamam.com/uri_stuff',
-        )
-      }
+    let(:recipe) { Recipe.new(
+      'Rotten Strawberry Stew',
+      'https://www.rottenstrawberries.com/image.jpg',
+      'http://www.edamam.com/uri_stuff',
+      )
+    }
 
     it 'recipe must have all attributes' do
       recipe.label.must_equal 'Rotten Strawberry Stew'
       recipe.image.must_equal 'https://www.rottenstrawberries.com/image.jpg'
       recipe.uri.must_equal 'http://www.edamam.com/uri_stuff'
     end
-    # Think about arrays vs. strings
-
-    it 'recipe can be missing an image' do
-      recipe.instance_variable_set(:@image, '')
-
-      recipe.image.must_equal ''
-    end
-
-    it 'recipe can be missing a name' do
-      recipe.instance_variable_set(:@label, '')
-
-      recipe.label.must_equal ''
-    end
-
-    it 'recipe can be missing a uri' do
-      recipe.instance_variable_set(:@uri, '')
-
-      recipe.uri.must_equal ''
-    end
 
     it 'recipe can not be made without all attributes' do
       proc { Recipe.new }.must_raise ArgumentError
     end
-
   end
 
   describe 'self.search' do
-    it 'search results returns an array' do
+    it 'valid search results returns an array' do
       VCR.use_cassette('recipes') do
         recipes = Recipe.search('arugula')
         recipes.class.must_equal Array
       end
     end
-    #NOTE: Fixed typo for next commit.
 
-    it 'search term without any results returns an empty array' do
+    it 'invalid search term returns empty array' do
+      VCR.use_cassette('recipes') do
+        recipes = Recipe.search('mksdjfhgskdghlsjdffs')
+        recipes.must_equal []
+      end
+    end
+  end
+
+  describe 'self.show_recipe' do
+    it 'valid recipe returns hash' do
+      VCR.use_cassette('recipes') do
+        recipe = Recipe.show_recipe('recipe_5e7b278b580ccea8ab485607dff5416a')
+        recipe.class.must_equal Hash
+      end
     end
 
-    it 'empty search term causes a redirect' do
+    it 'invalid uri returns an empty hash' do
+      VCR.use_cassette('recipes') do
+        recipe = Recipe.show_recipe('recipe_gfljkhfsgjlhksfgkjhdsfgfghf')
+        recipe.must_equal {}
+      end
     end
   end
 end
