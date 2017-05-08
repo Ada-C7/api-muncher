e95efd55242b7eaed64f0ebb2facac5a93dd7991
Ann Dai <gofarann@gmail.com>
2017-05-08 11:49:11 -0700

    accessed using JSON

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -11,7 +11,7 @@ describe SearchesController do
 
   it "should get the result list page" do
     VCR.use_cassette("munch") do
-      get results_path
+      get results_path(q: "apple")
       must_respond_with :success
     end
   end
@@ -19,6 +19,13 @@ describe SearchesController do
   it "should get the show recipe page" do
     VCR.use_cassette("munch") do
       get recipe_path("2e10a939a6a800da90f4460be18b4851")
+      must_respond_with :success
+    end
+  end
+
+  it "should get filtered diet result page" do
+    VCR.use_cassette("munch") do
+      get diet_path(id: "celery", label: "Low-Carb")
       must_respond_with :success
     end
   end
