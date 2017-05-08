6a17632685132955b16bff44793b0b77ce9f1db6
Ting Wong <hellotingwong@gmail.com>
2017-05-08 12:02:33 -0700

    contorller testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'recipes#index'
-  post '/', to: 'recipes#search'
+  post '/', to: 'recipes#search', as: 'search'
   get 'recipes/:id', to: 'recipes#show', as: 'recipe'
 end
