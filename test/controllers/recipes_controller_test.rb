f04d4c8492e65f7af0d54cd8497d406dc9bb3d8a
Anacelia Gillette <acgillette426@gmail.com>
2017-05-08 16:00:13 -0500

    Finishing up testing

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -6,10 +6,18 @@ describe RecipesController do
     must_respond_with :success
   end
 
-      it "should get a list of recipes" do
-        VCR.use_cassette("get_recipes") do
-          get get_recipes_path, params: {"search" => "chicken", page: "1"}
-        end
-        must_respond_with :success
-      end
+  it "should get a list of recipes" do
+    VCR.use_cassette("get_recipes") do
+        get get_recipes_path, params: {"search" => "chicken", page: "1"}
+    end
+      must_respond_with :success
+    end
+
+  it "should get an individual recipe" do
+    VCR.use_cassette("get_recipe") do
+      response = EdamamApiWrapper.findRecipes("chicken")[0]
+      get get_recipe_path(response.label), params: {"uri" => response.uri}
+    end
+    must_respond_with :success
+  end
 end
