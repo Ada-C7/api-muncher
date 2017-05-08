62f0e53d5940cd7ecfefee7f431f64461c5b56d1
Theresa Manney <theresa.m.17@gmail.com>
2017-05-07 21:10:24 -0700

    working on tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 describe RecipesController do
+  before do
+    VCR.insert_cassette("recipes-controller")
+  end
+  after do
+    VCR.eject_cassette("recipes-controller")
+  end
+  
   it "should get index" do
     get recipes_index_url
     value(response).must_be :success?
