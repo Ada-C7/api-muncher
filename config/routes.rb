0f42729d1dd3b4044d0c151ef1c1cc534b56c5b4
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-08 12:32:19 -0700

    edamam attribution, link in seperate tab

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'searches#index'
-
-  # get '/searches', to: 'searches#index', as: 'index'
   get '/searches/show', to: 'searches#show', as: 'show'
 end
