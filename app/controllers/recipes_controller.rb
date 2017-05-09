cd19b69209fe30124d98110efbcddc8516abd4f7
Cara Comfort <cara.comfort10@gmail.com>
2017-05-08 17:00:39 -0700

    Add error messages for when no results are found

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,15 +8,29 @@ class RecipesController < ApplicationController
   def index
     params = recipe_params
     @search_term = params["q"]
-    @recipes = Recipe.search(params)
-    @count = Recipe.count_results(params)
     @page = (params["to"] ||= 12).to_i / 12
 
+    begin
+      @recipes = Recipe.search(params)
+      @count = Recipe.count_results(params)
+    rescue RecipeError => error
+      flash[:error_message] = "Application Error: #{error}."
+    end
+
+    if @recipes.empty?
+      flash[:error_message] = "No results found. Please try again."
+      redirect_to root_path
+    end
   end
 
   def show
     @search_term = params["q"]
-    @recipe = Recipe.search("q" => params["id"]).first
+
+    begin
+      @recipe = Recipe.search("q" => params["id"]).first
+    rescue RecipeError => error
+      flash[:error_message] = "Application Error: #{error}."
+    end
   end
 
   private
