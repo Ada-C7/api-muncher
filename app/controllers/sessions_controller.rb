5817fd6138ab21a452b2eaac90c930fa1b81358e
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-09 02:00:15 -0700

    trying to fix heroku google oauth error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,9 @@
 class SessionsController < ApplicationController
-  def create
-    auth_hash = request.env['omniauth.auth']
-    user = User.find_by(provider: params[:provider],
-                      uid: auth_hash["uid"])
 
-    if user.nil?
-      #Don't know this user - build a new one
-      user = User.from_omniauth(auth_hash)
-      if user.save
-        session[:user_id] = user.id
-        flash[:message] = "Successfully logged in as user #{user.name}"
-      else
-        flash[:message] = "Could not log in"
-        user.errors.messages.each do |field, problem|
-          flash[:field] = problem.join(', ')
-        end
-      end
-    else
+  def create
+      user = User.from_omniauth(env["omniauth.auth"])
       session[:user_id] = user.id
-      flash[:message] = "Successfully logged in as user #{user.name}"
-    end
-    redirect_to root_path
+      redirect_to root_path
   end
 
   def destroy
