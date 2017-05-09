816ac5164cc91438115a460f6fc15f8e350417ce
Kelly Souza <kellysouza99@gmail.com>
2017-05-08 19:55:30 -0700

    recipe testing

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -2,26 +2,24 @@ require "test_helper"
 
 describe Recipe do
 
- it "requires recipe label " do
-   options = {
-     label: nil,
-     image: "www.url.com",
-     uri: "www.recipe.com"
-   }
-   proc {
-     Recipe.new(options)
-   }.must_raise ArgumentError
-   end
+  it "requires recipe label " do
+    options = {
+      label: nil,
+      image: "www.url.com",
+      uri: "www.recipe.com"
+    }
+    proc {
+      Recipe.new(options)
+    }.must_raise ArgumentError
+  end
 
-   it "creates recipe with valid options" do
-     options = {
-       label: "Food",
-       image: "www.url.com",
-       uri: "www.recipe.com"
-     }
-      recipe = Recipe.new(options)
-      recipe.must_be_instance_of Recipe
-     end
-
-
+  it "creates recipe with valid options" do
+    options = {
+      label: "Food",
+      image: "www.url.com",
+      uri: "www.recipe.com"
+    }
+    recipe = Recipe.new(options)
+    recipe.must_be_instance_of Recipe
+  end
 end
