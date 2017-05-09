d7aa212a6b05a5a99f801def2250f20075091233
Kaitlin Ramirez <rmrz.kt@gmail.com>
2017-05-09 01:44:17 -0700

    a couple more tests

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,7 +1,21 @@
 require "test_helper"
 
 describe SearchesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    it "shows the homepage" do
+      get root_path
+      must_respond_with :success
+    end
+  end
+
+  describe "show" do
+    it "gives shows a recipe" do
+      skip
+      VCR.use_cassette("results") do
+        uri = "http://www.edamam.com/ontologies/edamam.owl#recipe_f1c853a77986214680bbdd424883499a"
+        get show_path
+        must_respond_with :success
+      end
+    end
+  end
 end
