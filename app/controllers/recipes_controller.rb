a6f783f661c033cb65ec03b74d55b9e6cd1e1607
Janice Lichtman <jalichtman@gmail.com>
2017-05-08 21:33:49 -0700

    finished tests for recipe controller (so far)

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,27 +5,26 @@ class RecipesController < ApplicationController
   def welcome
   end
 
+
   # List the recipes from a given search
   def index
-    begin
-      @recipes, @total_count  = Recipe.search(params[:search],params[:from],params[:to])
+    @recipes, @total_count  = Recipe.search(params[:search],params[:from],params[:to])
 
-      if @recipes.empty?
-        if params[:search] == ""
-          flash[:message] = "Gimmie something to work with if you want some recipes."
-        else
-          flash[:message] = "Sorry, we have no recipes for #{params[:search]}."
-        end
-        return   @recipes, @total_count
+    if @recipes.empty?
+      if params[:search] == ""
+        flash[:message] = "Gimmie something to work with if you want some recipes."
+      else
+        flash[:message] = "Sorry, we have no recipes for #{params[:search]}."
       end
-    rescue ArgumentError
-      flash[:message] = "An internal error occured."
-      render :index
+      return   @recipes, @total_count
     end
   end
 
   def show
     @recipe = Recipe.getRecipe(params[:recipe])
+    if @recipe == nil
+      flash[:message] = "Sorry, we cannot find that recipe."
+    end
   end
 
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -25,39 +25,29 @@ describe RecipesController do
     end
 
     it "succeds and gives an appropriate message when no recipes are found" do
-       get recipes_path, params: {search: "XXXXXX", from:0, to:12}
-       flash[:message].must_equal  "Sorry, we have no recipes for XXXXXX."
+      get recipes_path, params: {search: "XXXXXX", from:0, to:12}
+      flash[:message].must_equal  "Sorry, we have no recipes for XXXXXX."
       must_respond_with :success
-     end
+    end
 
-     it "succeds and gives an appropriate message when an empty search was given" do
-        get recipes_path, params: {search: "", from:0, to:12}
-        flash[:message].must_equal  "Gimmie something to work with if you want some recipes."
-       must_respond_with :success
-      end
-
-      it "succeds and gives an appropriate message when an empty search was given" do
-         get recipes_path, params: {search: "", from:0, to:12}
-         flash[:message].must_equal  "Gimmie something to work with if you want some recipes."
-        must_respond_with :success
-       end
-
+    it "succeds and gives an appropriate message when an empty search was given" do
+      get recipes_path, params: {search: "", from:0, to:12}
+      flash[:message].must_equal  "Gimmie something to work with if you want some recipes."
+      must_respond_with :success
+    end
   end
-  #   it "succeeds with no users" do
-  #     # Start with a clean slate
-  #     Vote.destroy_all # for fk constraint
-  #     User.destroy_all
-  #
-  #     get users_path
-  #     must_respond_with :success
-  #   end
-  # end
 
   describe "show" do
     it "succeeds when a recipe is returned" do
       get recipe_path("c92c402e57d06df096adbffc393010c5")
       must_respond_with :success
     end
-  end
 
+    it "succeds and gives an appropriate message when a recipe is not found" do
+      get recipe_path("c92c402e57d06df096adbffc393010c5XXXXX")
+      flash[:message].must_equal  "Sorry, we cannot find that recipe."
+      must_respond_with :success
+    end
+  end
+  
 end
