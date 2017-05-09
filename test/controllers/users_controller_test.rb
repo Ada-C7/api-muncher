0d3df0f6d64190a65b7e8416d73cdb913b57772d
Anna Elisabeth <anna.e.boyd@gmail.com>
2017-05-09 09:01:26 -0700

    added tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,10 +4,36 @@ describe UsersController do
   it "creates a new user" do
     start_count = User.count
     user = User.new(provider: "google", uid: 99999, name: "test_user", oauth_token: "string of thigns", oauth_expires_at: Time.new(2018))
-    OmniAuth.config.mock_auth[:google] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-    get auth_callback_path(:google)
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get "/auth/google_oauth2/callback"
     must_redirect_to root_path
 
     User.count.must_equal start_count + 1
+  end
+
+  it "doesn't log in a bad user" do
+
+
+  end
+
+  it "logs in an existing user" do
+    start_count = User.count
+    user = users(:wolf)
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get "/auth/google_oauth2/callback"
+
+    must_redirect_to root_path
+    User.count.must_equal start_count
+  end
+
+  it "logs out an existing user" do
+    start_count = User.count
+    user = users(:wolf)
+    OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get "/auth/google_oauth2/callback"
+
+    get "/signout"
+    must_redirect_to root_path
+    User.count.must_equal start_count
   end
 end
