8c4b0f113ec58eb0215f227a9eb515b67d833ea5
Hyunji Kim <ricecakemonster@gmail.com>
2017-05-09 09:52:46 -0700

    session[:search_terms]=[] when user signs out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:status] = :success
     flash[:result_text] = "Successfully logged out"
+    session[:search_terms] = []
     redirect_to root_path
   end
 end
