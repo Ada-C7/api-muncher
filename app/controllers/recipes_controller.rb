0dc4f27f842645cbf6d287578895ace9ccc1914a
Haby Randall <sow.haby@gmail.com>
2017-05-10 18:51:57 -0700

    Overriding some of foundation's styling

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   def index
     if params[:q]
       @recipes = EdamamApiWrapper.search(params[:q])
-      @recipes_pages = @recipes.paginate(:page => params[:page], :per_page => 10)
+      @recipes_pages = @recipes.paginate(:page => params[:page], :per_page => 12)
 
     end
   end
